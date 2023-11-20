0f85549530da688551298e70988dce4f85a3a4e1
jacky <jacky@fuba.tech>
2023-11-20 09:04:50 +0800

    add: login enter

--- resources/js/login.js
+++ resources/js/login.js
@@ -1,9 +1,35 @@
 var flag_password = false;
 var flag_pwd2 = false;
-//主程式
+
+// 主程式
 $(function () {
-    //按鈕監聽
+    // 按鈕監聽
     $("#Login_btn").bind("click", function () {
+        login();
+    });
+
+    // 即時監聽 密碼
+    $("#Login_Password").bind("input propertychange", function () {
+        validatePassword();
+    });
+
+    // 即時監聽 確認密碼
+    $("#Login_pwd2").bind("input propertychange", function () {
+        validateConfirmPassword();
+    });
+
+    // Enter 鍵監聽
+    $("#Login_Password, #Login_pwd2").on("keypress", function (e) {
+        if (e.keyCode === 13) {
+            login();
+        }
+    });
+
+    $("#Login_Register").on("click", function () {
+        window.location.href = "/register";
+    });
+
+    function login() {
         if (flag_password && flag_pwd2) {
             var dataJSON = {
                 Email: $("#Login_Email").val(),
@@ -25,44 +51,35 @@ $(function () {
                     }
                 },
                 error: function (xhr, status, error) {
+                    // 处理错误
                 }
             });
         } else {
             alert("輸入資料不符合規定!!!");
         }
-    });
+    }
 
-    //即時監聽    密碼
-    $("#Login_Password").bind("input propertychange", function () {
+    function validatePassword() {
         if ($("#Login_Password").val().length < 9 && $("#Login_Password").val().length > 3) {
             $("#err_password").html("符合規定");
             $("#err_password").css("color", "#0E0");
             flag_password = true;
-        }
-        else {
+        } else {
             $("#err_password").html("字數不符合規定!");
             $("#err_password").css("color", "#F00");
             flag_password = false;
         }
+    }
 
-    });
-
-    //即時監聽    確認密碼
-    $("#Login_pwd2").bind("input propertychange", function () {
+    function validateConfirmPassword() {
         if ($("#Login_pwd2").val() == $("#Login_Password").val()) {
             $("#err_pwd2").html("與密碼相同");
             $("#err_pwd2").css("color", "#0E0");
             flag_pwd2 = true;
-        }
-        else {
+        } else {
             $("#err_pwd2").html("與密碼不符合");
             $("#err_pwd2").css("color", "#F00");
             flag_pwd2 = false;
         }
-    });
-
-    $("#Login_Register").on("click", function () {
-        window.location.href = "/register";
-    });
-
+    }
 });
